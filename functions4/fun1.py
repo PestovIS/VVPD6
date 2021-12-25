e1905b0349a1d3e33e213d3d12b39e49e25cec7d
Pestov <pestov_04091@mail.ru>
2021-12-25 21:01:55 +0700

    изменения для 100% coverage

--- functions4/fun1.py
+++ functions4/fun1.py
@@ -6,14 +6,23 @@
 
 def is_gray(color):
     checklist = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F")
-
-    if color[0] != '#' or len(color) != 7:
-        return None
-    for i in color[1:]:
-        if i not in checklist:
+    if type(color) is str:
+        if color[0] != '#' or len(color) != 7:
             return None
+        for i in color[1:]:
+            if i not in checklist:
+                return None
+    else:
+        return None
 
     if color[1:3] == color[3:5] == color[-2:]:
         return True
     else:
         return False
+
+
+is_gray(0)
+is_gray('#111111')
+is_gray('#111110')
+is_gray('asdf')
+is_gray('#11111N')
--- functions4/fun2.py
+++ functions4/fun2.py
@@ -2,10 +2,28 @@ def to_gray(color):
     """
     Концентрации зеленого и синего подстраиваются под концентрацию красного, чтобы концентрации были равны.
     """
+    checklist = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F")
+    if type(color) is str:
+        if color[0] != '#' or len(color) != 7:
+            return None
+        for i in color[1:]:
+            if i not in checklist:
+                return None
+    else:
+        return None
+
     red = int(color[1:3], 16) - int(color[1:3], 16)
     green = int(color[1:3], 16) - int(color[3:5], 16)
     blue = int(color[1:3], 16) - int(color[-2:], 16)
+    if red == green == blue:
+        return None
 
     return red, green, blue
 
-print(to_gray('#F1AAB5'))
+
+to_gray(1243)
+to_gray('#FFFFFF')
+to_gray('FFFFFF')
+to_gray('#FFFFFM')
+to_gray('#F88954')
+
